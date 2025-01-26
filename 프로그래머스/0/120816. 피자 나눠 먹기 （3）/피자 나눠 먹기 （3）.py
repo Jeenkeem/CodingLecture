7c294a995503f39c182a4b0bf962d4dc58de641d
Jeenkeem <146731683+Jeenkeem@users.noreply.github.com>
2025-01-27 04:38:54 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,11 +1,10 @@
 def solution(slice, n):
     answer = 0
-    
-    if n%slice < slice:
-        if n%slice == 0:
-            answer = n//slice
-        else:
-            answer = n//slice + 1
+
+    if n%slice == 0:
+        answer = n//slice
+    else:
+        answer = n//slice + 1
     
     
     return answer
